e62d820f5b785e096077e73655627c174eb8a3db
Marguerite Teulon <marguerite.teulon@gmail.com>
2021-02-27 12:20:10 +0100

    controler.py clean and comment modele.py clean and add tournament score function

--- controler.py
+++ controler.py
@@ -2,19 +2,27 @@ from modele import *
 
 # For new tournament, enter all information about it
 tournament = Tournament(name=input("name"), player_number=input("number_of_player"),
-                        round_number=input("number of round"), exit_player=None, round_name=None, players=[],
-                        sorted_players=[], tournament_score=0)
+                        round_number=input("number of round"), exit_player=None, round_name=[], players=[],
+                        sorted_players=[])
 
-for player in range(0, tournament.player_number):
-    player = Player(name=input("last_name"), initial_score=input("score_initial"), tournament_score=0, match_score=[])
-    tournament.list_of_players(player)
+# for the tournament, enter information for x player (player_number)
+for i in range(0, tournament.player_number):
+    # enter all information about player
+    tournament.list_of_players(Player(first_name=input("first_name"), last_name=input("last_name"),
+                                      initial_score=input("score_initial")))
 
-tournament.sorted_players_list_beginning(tournament.sorted_players, tournament.players)
+# classification of players by initial score
+tournament.sorted_players_list_beginning()
 
+# for number of round : enter the name of the round, match players, add the score of each of them
 for x in range(0, tournament.round_number):
-    tournament.name_of_the_round(tournament.round_name)
-    tournament.matching_players(tournament.players, tournament.player_number, tournament.sorted_players,
-                                tournament.list_of_match)
-    tournament.score_of_match(tournament.players, tournament.player_number, tournament.list_of_match,
-                              tournament.exit_player)
-    tournament.sort_list_other_round(tournament.sorted_players, tournament.players, tournament.round_name)
+    tournament.name_of_the_round()
+    tournament.matching_players()
+    tournament.score_of_match()
+    # if this is the last round then display results of tournament and stop the app
+    if x == tournament.round_number - 1:
+        tournament.tournament_classement()
+        break
+    # if it's not the last round then classify players by score at the previous round and continues
+    else:
+        tournament.sort_list_other_round()
--- modele.py
+++ modele.py
@@ -3,132 +3,157 @@ from random import *
 
 
 class Player:
-
+    """ function to complete for a new player to be record in the list while a player already record isn't
     @classmethod
     def all_players(cls):
         all_players = [Player.__init__()]
-        print(all_players)
+        print(all_players)"""
 
-    def __init__(self, name, initial_score, tournament_score, match_score=[]):
-        self.name = name
+    # function to create class instance
+    def __init__(self, first_name, last_name, initial_score):
+        self.first_name = first_name
+        self.last_name = last_name
         self.initial_score = int(initial_score)
-        self.tournament_score = tournament_score
-        self.match_score = match_score
 
-    def information_about_player(self):
-        print("Mon nom est ", self.name, " mon score initial est ", self.initial_score, " et mon score au match est ",
-              self.match_score)
-
-    """# class Players function to allow verification of information about each player
-    def information_about_players(self):
-        global player
-        player = {"full_name": self.name, "initial_score": self.initial_score}
-        print("Mon nom est", player["full_name"], "et mon score : ", player["initial_score"])
-
-    # Add players to the list
-    def __add__(self):
-        global player_list
-        player_list.append(player)
-        print(player_list)
-        """
-
-    """# add score
-    def score_match(self):
-        for player in player_"""
+    """def information_about_player(self):
+        print("Mon nom est ", self.name, " mon score initial est ", self.initial_score)"""
 
 
-    def score_tournament(self, score_match, tournament_score):
-        tournament_score = tournament_score + score_match
-
-
-class Tournament:
-    # creation of object
-    def __init__(self, name, player_number, round_number, exit_player, round_name=None, all_rounds_names=[], players=[], sorted_players=[], tournament_score=0,
-                 list_of_match={}):
+class Tournament(Player):
+    # creation of instance Tournament
+    def __init__(self, name, player_number, round_number, exit_player, round_name=[], players=[],
+                 sorted_players=[], list_of_match={}, total_score_of_tournament=0):
         self.name = name
         self.player_number = int(player_number)
         self.round_number = int(round_number)
         self.exit_player = exit_player
         self.round_name = round_name
-        self.all_rounds_names = all_rounds_names
         self.players = players
         self.sorted_players = sorted_players
-        self.tournament_score = tournament_score
         self.list_of_match = list_of_match
+        Player.total_score_of_tournament = total_score_of_tournament
 
-
-    # players' list for a tournament
+    # players' list for a tournament filled with the players who participates to the tournament
     def list_of_players(self, Player):
-        self.players.append({"full_name": Player.name, "initial_score": Player.initial_score})
-        #print(self.players)
+        self.players.append({"full_name": Player.first_name + Player.last_name,
+                             "score initial": Player.initial_score})
 
-    # sorted players' list by rank
-    def sorted_players_list_beginning(self, sorted_players, players):
-        sorted_players.extend(sorted(players, key=lambda k: k["initial_score"], reverse=True))
-        print(sorted_players)
+    # sorted players' list by : players are classify with their initial score
+    def sorted_players_list_beginning(self):
+        self.sorted_players.extend(sorted(self.players, key=lambda k: k["score initial"], reverse=True))
+        # print(sorted_players)
 
-    def name_of_the_round(self, round_name):
-        self.round_name = input("named the round")
-        print(self.round_name)
+    # record the name of each round of the tournament in a list
+    def name_of_the_round(self):
+        self.round_name.append(input("named the round"))
+        # print(self.round_name)
 
-    # association of two players for the first round and display all match for the round
-    def matching_players(self, player, player_number, sorted_players, list_of_match):
-        # Divide the sorted list in two in order to match each player of the first list with a player of the
-        # second one according to their rank in the lists
-        # if number_of_players isn't pair exit one player randomly
-        if (player_number % 2) != 0:
-            half_player = (player_number - 1) / 2
-            exit_player = choice(sorted_players)
-            sorted_players.remove(exit_player)
-            # if number_of_players is pair
-            return exit_player, half_player
+    # this function create pair of player (a pair = one match)
+    def matching_players(self):
+        # two possibilities : player number is pair or not
+        if (self.player_number % 2) != 0:  # if number_of_players isn't pair exit one player randomly
+            half_player = (self.player_number - 1) / 2
+            self.exit_player = choice(self.sorted_players)
+            self.sorted_players.remove(self.exit_player)
+            return self.exit_player, half_player
             # print(exit_player)
-        else:
-            half_player = player_number / 2
+        else:  # if player' number is pair
+            half_player = self.player_number / 2
 
-        # Creation of the two lists
-        s1 = sorted_players[0: int(half_player)]
-        s2 = sorted_players[int(half_player):]
+        # sorted list is divide in two lists s1 and s2
+        s1 = self.sorted_players[0: int(half_player)]
+        s2 = self.sorted_players[int(half_player):]
 
-        # list of all matches of a round
+        # all matches of a round record in the same list, list_of_match allow to access matches of each round
+        self.list_of_match[self.round_name[len(self.round_name) - 1]] = []
+        if len(self.round_name) == 1:  # If first round then just need to pair players
+            for i in range(0, int(half_player)):
+                match = [s1[i], s2[i]]  # take players of each list to pair them by rank in the list
+                self.list_of_match[self.round_name[0]].append(match)
+            # print(list_of_match[self.round_name[0]])
+            return self.list_of_match[self.round_name[0]]
 
-        list_of_match[self.round_name] = []
-        for i in range(0, int(player_number / 2)):
-            match = [s1[i], s2[i]]
-            list_of_match[self.round_name].append(match)
-        return list_of_match[self.round_name]
-        print(list_of_match[self.round_name])
+        else:  # if it's not the first round, need to pair players and to verify that they don't already play together
+            i = 0
+            while i < int(half_player):  # as long as there is still players to match
+                match = [s1[i], s2[i]]
+                # if match between to players already happen then need to pair with next player :
+                if match in self.list_of_match[self.round_name[(len(self.round_name)) - 2]]:
+                    match1 = [s1[i], s2[i + 1]]
+                    self.list_of_match[self.round_name[len(self.round_name) - 1]].append(match1)
+                    match2 = [s1[i + 1], s2[i]]
+                    self.list_of_match[self.round_name[len(self.round_name) - 1]].append(match2)
+                    if i + 1 == int(half_player) - 1:  # if there is no player left then stop the matching
+                        break
+                    else:  # if there is still player then switched to next players unpaired
+                        i = i + 2
+                #  if match never happen, directly record it in the list
+                else:
+                    self.list_of_match[self.round_name[len(self.round_name) - 1]].append(match)
 
-    def score_of_match(self, players, player_number, list_of_match, exit_player):
+        # print(list_of_match[self.round_name[len(self.round_name) - 1]])
+        return self.list_of_match[self.round_name[len(self.round_name) - 1]]
 
+    def score_of_match(self):
         # add point for a round to each player
-        players[:] = []
-        for match in list_of_match[self.round_name]:
+        # empty the list of players to fill it again with the score of the last round added
+        self.players[:] = []
+        # for each match record in the list of match of the round
+        for match in self.list_of_match[self.round_name[len(self.round_name) - 1]]:
+            # add point to first player of a match
             print(match[0]["full_name"])
-            match[0][self.round_name] = input("Entrer le score")
-            players.append(match[0])
+            match[0][self.round_name[len(self.round_name) - 1]] = float(input("Entrer le score"))
+            # Add the point of the round to total score
+            if len(self.round_name) > 1:  # in case it's not the first round : add point to the total
+                match[0]["total_score_of_tournament"] = match[0]["total_score_of_tournament"] + \
+                                                        match[0][self.round_name[len(self.round_name) - 1]]
+            else:  # in case this is the first create the key and add point of first round
+                match[0]["total_score_of_tournament"] = match[0][self.round_name[len(self.round_name) - 1]]
+            # add back the player to the list
+            self.players.append(match[0])
+
+            # add point to the second player of the match
             print(match[1]["full_name"])
-            match[1][self.round_name] = input("Entrer le score")
-            players.append(match[1])
-        if (player_number % 2) != 0:
-            exit_player[self.round_name] = 0
-            players.append(exit_player)
-        else:
+            match[1][self.round_name[len(self.round_name) - 1]] = float(input("Entrer le score"))
+            # Add the point of the round to total score
+            if len(self.round_name) > 1:   # in case it's not the first round : add point to the total
+                match[1]["total_score_of_tournament"] = match[1]["total_score_of_tournament"] + \
+                                                        match[1][self.round_name[len(self.round_name) - 1]]
+            else:  # in case this is the first create the key and add point of first round
+                match[1]["total_score_of_tournament"] = match[1][self.round_name[len(self.round_name) - 1]]
+            # add back the player to the list
+            self.players.append(match[1])
+
+        if (self.player_number % 2) != 0:  # if player number isn't pair, add zero point to exit player
+            self.exit_player[self.round_name[len(self.round_name) - 1]] = 0
+            # Add the point of the round to total score
+            if len(self.round_name) > 1:  # in case it's not the first round : add point to the total
+                self.exit_player["total_score_of_tournament"] = self.exit_player["total_score_of_tournament"] + \
+                                                                self.exit_player[
+                                                                    self.round_name[len(self.round_name) - 1]]
+            else: # in case this is the first create the key and add point of first round
+                self.exit_player["total_score_of_tournament"] = self.exit_player[
+                    self.round_name[len(self.round_name) - 1]]
+            # this player is add back to the players list
+            self.players.append(self.exit_player)
+        else:  # if player number is pair then pass
             pass
-        for player in players:
+        """for player in self.players:
+            print(player["full_name"], player["total_score_of_tournament"])"""
+
+    # function to classify players by their score at the last round
+    def sort_list_other_round(self):
+        # empty the list to create a new one
+        self.sorted_players[:] = []
+        # classify players by previous round's score
+        self.sorted_players.extend(sorted(self.players, key=lambda k: k[self.round_name[len(self.round_name) - 1]],
+                                          reverse=True))
+        # print(sorted_players)
+
+    # classify players by their total score at the tournament and give the classification (name and score)
+    def tournament_classement(self):
+        self.sorted_players[:] = []
+        self.sorted_players.extend(sorted(self.players, key=lambda k: k["total_score_of_tournament"], reverse=True))
+        for player in self.sorted_players:
             print(player)
-
-    def sort_list_other_round(self, sorted_players, players, round_name):
-        sorted_players[:] = []
-        sorted_players.extend(sorted(players, key=lambda k: k[self.round_name], reverse=True))
-        print(sorted_players)
-
-    def tournament_score(self, players):
-        for player in players:
-            print(self.tournament_score)
-
-
-
-
-
-
+            print(player["total_score_of_tournament"])
